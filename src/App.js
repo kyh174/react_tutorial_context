9c7cbca73ee5b38b9cfa69256431931d490f5cd5
kyh174 <kyh174@naver.com>
2024-03-15 17:35:04 +0900

    context 연습

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import Heading from './Heading';
+import Section from './Section';
+import './App.css';
+
+function App() {
+  return (
+    <Section>
+      <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+export default App;
